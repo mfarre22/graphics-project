80cd2f16289eb3dbbbada7f4106cf8ce5c05f8c3
Ross McIlvaine <rossmcilvaine@gmail.com>
2022-05-04 13:54:26 -0400

    Get rid of knocked pins

--- bowling-game.js
+++ bowling-game.js
@@ -113,6 +113,7 @@ function main() {
       pins[8].name = "Pin_9";
       pins[9] = root.getObjectByName("Bowling_Pin008_12"); // Pin 10
       pins[9].name = "Pin_10";
+      console.log(pins[1].position.y);
 
       // compute the box that contains all the stuff
       // from root and below
@@ -342,6 +343,7 @@ function main() {
         for(let i = 0; i<10; i++) {
           pins[i].rotation.y = 0;
           pins[i].rotation.z = 0;
+          pins[i].position.y = 0.10000000149011612;
         }
       };
       color.addEventListener('input', function(e) {
@@ -420,6 +422,10 @@ function main() {
         for(let i = 0; i<10; i++) {
           if(pins[i].rotation.y == -1.5 && pins[i].rotation.z == 1.5) {
             count++;
+            // If on second throw, get rid of pins that were knocked over
+            if(throwNum%2 == 1){
+              pins[i].position.y +=1000;
+            }
           }
         }
       }
